68064b2cc651c6765be00f61b507b94b56fe7451
Maciej Wiatr <maciekwiatr17@gmail.com>
2020-12-22 01:59:55 +0100

    Fix invalid merge

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { withReduxCookiePersist } from "next-redux-cookie-wrapper";
 import { Provider } from "react-redux";
-import { wrapper } from "../store";
-import { useStore } from "react-redux";
+import makeStore from "../store";
 import "../styles/globals.css";
 import COOKIES from "cookies";
 
 if (process.env.NODE_ENV !== "production") console.log(COOKIES);
 
-function MyApp({ Component, pageProps }) {
-    const store = useStore((state) => state);
+function MyApp({ Component, pageProps, store }) {
     return (
         <Provider store={store}>
             <ChakraProvider>
@@ -18,4 +17,10 @@ function MyApp({ Component, pageProps }) {
     );
 }
 
-export default wrapper.withRedux(MyApp);
+export default withReduxCookiePersist(makeStore, {
+    cookieConfig: {
+        expiration: {
+            default: 365 * 86400,
+        },
+    },
+})(MyApp);
--- src/store/index.js
+++ src/store/index.js
@@ -1,80 +1,11 @@
-import rootReducer from "./slices/index";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./slices";
 
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-
-import { createWrapper, HYDRATE } from "next-redux-wrapper";
-
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from "redux-persist";
-
-import createWebStorage from "redux-persist/lib/storage/createWebStorage";
-
-const createNoopStorage = () => {
-    return {
-        getItem(_key) {
-            return Promise.resolve(null);
-        },
-        setItem(_key, value) {
-            return Promise.resolve(value);
-        },
-        removeItem(_key) {
-            return Promise.resolve();
-        },
-    };
+const makeStore = (initialState, options) => {
+    return configureStore({
+        reducer: rootReducer,
+        preloadedState: initialState,
+    });
 };
 
-const storage =
-    typeof window !== "undefined"
-        ? createWebStorage("local")
-        : createNoopStorage();
-
-const makeStore = ({ isServer }) => {
-    if (isServer) {
-        //If it's on server side, create a store
-        return configureStore({
-            reducer: rootReducer,
-        });
-    } else {
-        //If it's on client side, create a store which will persist
-        // const storage = storage.default
-
-        const persistConfig = {
-            key: "root",
-            version: 1,
-            blacklist: [],
-            storage,
-        };
-
-        const persistedReducer = persistReducer(persistConfig, rootReducer); // Create a new reducer with our existing reducer
-
-        const store = configureStore({
-            reducer: persistedReducer,
-            middleware: getDefaultMiddleware({
-                serializableCheck: {
-                    ignoredActions: [
-                        FLUSH,
-                        REHYDRATE,
-                        PAUSE,
-                        PERSIST,
-                        PURGE,
-                        REGISTER,
-                    ],
-                },
-            }),
-        });
-
-        store.__persistor = persistStore(store);
-
-        return store;
-    }
-};
-
-export const wrapper = createWrapper(makeStore);
+export default makeStore;
