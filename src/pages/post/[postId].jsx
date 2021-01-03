56790219b00f05402c6d2499e7cc10250aa566e5
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-01-03 16:58:58 +0100

    Add post detail view

---
+++ src/pages/post/[postId].jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import Layout from "../../components/Layout";
+import PostDetail from "../../components/PostDetail";
+import { apiURL } from "../../constants";
+
+const PostDetailPage = ({ initialPost }) => {
+    const [post, setPost] = useState(initialPost);
+    const user = useSelector((state) => state.user.user);
+    return (
+        <Layout>
+            <div className="flex w-full h-full justify-center items-center p-2">
+                <PostDetail initialPost={post} />
+            </div>
+        </Layout>
+    );
+};
+
+export default PostDetailPage;
+
+export const getServerSideProps = async ({ params }) => {
+    const { postId } = params;
+
+    const postResp = await axios.get(`${apiURL}posts/${postId}`);
+
+    return {
+        props: {
+            initialPost: postResp.data,
+        },
+    };
+};
