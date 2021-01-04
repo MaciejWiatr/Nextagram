5c7ac03f9bf5c9358042e323b2ea5a3e5941e5e5
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-01-04 23:49:43 +0100

    Fix env variables issues

--- next.config.js
+++ next.config.js
@@ -2,8 +2,15 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
     enabled: process.env.ANALYZE === "true",
 });
 
+const dev_domains = ["", "127.0.0.1"];
+
+const prod_domains = [process.env.NEXT_PUBLIC_BACKEND_ROOT];
+
 module.exports = withBundleAnalyzer({
     images: {
-        domains: ["", "127.0.0.1", "local.wiatr.tech"],
+        domains:
+            !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+                ? dev_domains
+                : prod_domains,
     },
 });
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,7 @@
-export const backendURL = process.env.BACKEND_URL || "http://127.0.0.1:8000";
+export const backendURL =
+    !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+        ? "http://127.0.0.1:8000"
+        : process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export const apiURL = `${backendURL}/api/`;
 
