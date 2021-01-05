246d5380e314230617be52c8e0716d854374a747
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-01-05 18:14:32 +0100

    Update search page's ui

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { Spinner } from "@chakra-ui/react";
 import { apiURL } from "../constants";
 import Card from "../components/Card";
 
@@ -10,15 +11,23 @@ const { default: Layout } = require("../components/Layout");
 
 const Search = () => {
     const [results, setResults] = useState({ posts: [], users: [] });
+    const [loading, setLoading] = useState(false);
     const router = useRouter();
     const { q } = router.query;
 
     const handleSearch = async () => {
-        const respPosts = await axios.get(`${apiURL}posts/?search=${q}`);
-        const respUsers = await axios.get(
+        setLoading(true);
+        const searchPosts = await axios.get(`${apiURL}posts/?search=${q}`);
+        const searchUsers = await axios.get(
             `${apiURL}accounts/users/?search=${q}`
         );
+        const [respPosts, respUsers] = await Promise.all([
+            searchPosts,
+            searchUsers,
+        ]);
+
         setResults(() => ({ posts: respPosts.data, users: respUsers.data }));
+        setLoading(false);
     };
 
     useEffect(() => {
@@ -27,50 +36,70 @@ const Search = () => {
 
     return (
         <Layout>
-            <div className=" flex flex-row overflow-hidden w-full">
-                <div className="w-1/4 overflow-hidden" />
-                <div className="w-2/4 overflow-hidden flex flex-col items-center  p-2">
-                    <h1 className="text-3xl">Search results for {`"${q}"`}</h1>
-                    <div className="w-full text-left">
+            <div className=" flex flex-row overflow-hidden justify-center w-full">
+                <div className="w-full p-3 md:w-3/4 max-w-5xl overflow-hidden flex flex-col items-center">
+                    <h1 className="text-3xl mt-2 text-center font-bold">
+                        Search results for{" "}
+                        <span className="font-normal">{`"${q}"`}</span>
+                    </h1>
+                    <div className="w-full text-left mt-2 mb-2">
                         <h3 className="text-xl">Users</h3>
                     </div>
-                    <ul className="flex flex-row flex-nowrap w-full">
-                        {results.users.length > 0
-                            ? results.users.map((user) => (
-                                  <li key={user.id}>
-                                      <Link
-                                          href={`/profile/${user.id}`}
-                                          passHref
-                                      >
-                                          <a
-                                              href="/"
-                                              className="w-24 h-32 flex flex-col justify-center items-center"
-                                          >
-                                              <div className="w-20 h-20 overflow-hidden relative rounded-full">
-                                                  <Image
-                                                      src={user.profile.photo}
-                                                      layout="fill"
-                                                  />
-                                              </div>
-                                              {user.username}
-                                          </a>
-                                      </Link>
-                                  </li>
-                              ))
-                            : "No users found"}
+                    <ul className="flex flex-row flex-nowrap w-full h-full pt-2 bg-white rounded shadow overflow-x-scroll">
+                        {results.users.length > 0 ? (
+                            results.users.map((user) => (
+                                <li key={user.id}>
+                                    <Link href={`/profile/${user.id}`} passHref>
+                                        <a
+                                            href="/"
+                                            className="w-24 h-32 flex flex-col justify-center items-center overflow-hidden"
+                                        >
+                                            <div className="w-20 h-20 overflow-hidden relative rounded-full">
+                                                <Image
+                                                    src={user.profile.photo}
+                                                    layout="fill"
+                                                />
+                                            </div>
+                                            {user.username}
+                                        </a>
+                                    </Link>
+                                </li>
+                            ))
+                        ) : (
+                            <div className="w-full text-center">
+                                {loading ? (
+                                    <Spinner />
+                                ) : (
+                                    <div>
+                                        <p>: (</p>
+                                        <p>No users have been found</p>
+                                    </div>
+                                )}
+                            </div>
+                        )}
                     </ul>
-                    <div className="w-full text-left">
+                    <div className="w-full text-left mt-2 mb-2">
                         <h3 className="text-xl">Posts</h3>
                     </div>
-                    <div className="w-full flex justify-center flex-col items-center pl-2 pr-2 pb-3">
-                        {results.posts.length > 0
-                            ? results.posts.map((post) => (
-                                  <Card key={post.id} initialPost={post} />
-                              ))
-                            : "No posts found"}
+                    <div className="w-full flex justify-around flex-row flex-wrap items-evenly p-3 bg-white rounded shadow">
+                        {results.posts.length > 0 && !loading ? (
+                            results.posts.map((post) => (
+                                <Card key={post.id} initialPost={post} />
+                            ))
+                        ) : (
+                            <div className="w-full text-center">
+                                {loading ? (
+                                    <Spinner />
+                                ) : (
+                                    <div>
+                                        <p>: (</p>
+                                        <p>No posts have been found</p>
+                                    </div>
+                                )}
+                            </div>
+                        )}
                     </div>
                 </div>
-                <div className="w-1/4 overflow-hidden" />
             </div>
         </Layout>
     );
