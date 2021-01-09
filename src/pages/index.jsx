4f16ace4d81f6ed65e93f70c9341eb649def6b2b
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-01-09 19:14:28 +0100

    Split home screen into multiple components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,31 +1,25 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Input, Button, Spinner } from "@chakra-ui/react";
-import { BiSearchAlt } from "react-icons/bi";
-import Router from "next/router";
+import { Spinner } from "@chakra-ui/react";
 import Layout from "../components/Layout";
-import Card from "../components/Card";
 import { fetchPosts } from "../store/slices/PostSlice";
+import PostList from "../components/PostList";
+import WelcomeMessage from "../components/WelcomeMessage";
 
 export default function Home() {
     const posts = useSelector((state) => state.posts.postList);
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user);
-    const [inputVal, setInputVal] = useState("");
     const [loading, setLoading] = useState(false);
 
-    const handleSearch = () => {
-        Router.push(`/search/?q=${inputVal}`);
-    };
-
-    const updatePosts = async () => {
+    const updatePosts = () => {
         setLoading(true);
-        await dispatch(fetchPosts(user));
+        dispatch(fetchPosts(user));
         setLoading(false);
     };
 
-    useEffect(async () => {
-        await updatePosts();
+    useEffect(() => {
+        updatePosts();
     }, [user.token]);
 
     return (
@@ -33,46 +27,17 @@ export default function Home() {
             <div className="w-full min-h-full flex justify-center flex-col items-center pl-2 pr-2 pb-3">
                 {loading ? <Spinner /> : null}
                 {posts.length > 0 ? (
-                    posts.map((post) => (
-                        <Card
-                            key={post.id}
-                            initialPost={post}
-                            updateParentPostList={updatePosts}
-                        />
-                    ))
+                    <PostList
+                        posts={posts}
+                        updateParentPostList={updatePosts}
+                    />
                 ) : (
                     <div className="text-center flex flex-col items-center justify-center">
                         {!user.isAuthenticated ||
                         user.user.profile.follows.length > 0 ? (
                             <p>No posts found</p>
                         ) : (
-                            <div className="flex flex-col items-center justify-center w-1/2">
-                                <form className="flex text-xl">
-                                    <Input
-                                        placeholder="Search"
-                                        value={inputVal}
-                                        onChange={(e) =>
-                                            setInputVal(e.target.value)
-                                        }
-                                    />
-                                    <Button
-                                        className="ml-1"
-                                        type="submit"
-                                        onClick={(e) => {
-                                            e.preventDefault();
-                                            handleSearch();
-                                        }}
-                                    >
-                                        <BiSearchAlt />
-                                    </Button>
-                                </form>
-                                <br />
-                                <div className="text-gray-500">
-                                    This page is blank because you are not
-                                    following anyone. First try searching for
-                                    your friends using the search input above
-                                </div>
-                            </div>
+                            <WelcomeMessage />
                         )}
                     </div>
                 )}
