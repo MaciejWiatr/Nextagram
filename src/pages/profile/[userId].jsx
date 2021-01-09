1a8e9b850c54bea28f862d3b4e7b56a9a17d1569
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-01-09 19:12:24 +0100

    Split profile page into multiple components

--- src/pages/profile/[userId].jsx
+++ src/pages/profile/[userId].jsx
@@ -1,94 +1,24 @@
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import {
-    Box,
     Button,
-    FormLabel,
-    Input,
     Tab,
     TabList,
     TabPanel,
     TabPanels,
     Tabs,
-    Textarea,
 } from "@chakra-ui/react";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/dist/client/router";
 import { BsFillGridFill } from "react-icons/bs";
-import { AiOutlinePlusCircle } from "react-icons/ai";
 import { FaList } from "react-icons/fa";
 import PostList from "../../components/PostList";
-import { fetchUser, updateUser } from "../../store/slices/UserSlice";
+import { fetchUser } from "../../store/slices/UserSlice";
 import { apiURL } from "../../constants";
 import Layout from "../../components/Layout";
 import ProfileSummary from "../../components/ProfileSummary";
 import PostGrid from "../../components/PostGrid";
-
-const NewPostForm = ({ updatePosts, fetchProfile }) => {
-    const user = useSelector((state) => state.user);
-    const formRef = useRef(null);
-    const [postImage, setPostImage] = useState("");
-    const postImagePreview = useRef(null);
-
-    const uploadForm = async (formData) => {
-        try {
-            await axios({
-                method: "post",
-                url: `${apiURL}posts/`,
-                data: formData,
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                    Authorization: `Token ${user.token}`,
-                },
-            });
-            updatePosts();
-            fetchProfile();
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const formData = new FormData(formRef.current);
-        uploadForm(formData);
-        e.target.reset();
-    };
-
-    const loadFile = (event) => {
-        setPostImage(URL.createObjectURL(event.target.files[0]));
-    };
-
-    return (
-        <Box className="w-full p-3 rounded bg-white shadow mt-2 mb-2">
-            <h2 className="text-xl">Add new post</h2>
-            <form ref={formRef} onSubmit={handleSubmit}>
-                <FormLabel>Image</FormLabel>
-                <label className="w-full h-10 flex justify-center items-center bg-gray-200 rounded hover:bg-gray-300 cursor-pointer">
-                    <AiOutlinePlusCircle />
-                    <Input
-                        type="file"
-                        className="p-1 hidden"
-                        name="image"
-                        onChange={loadFile}
-                    />
-                </label>
-                {postImage !== "" ? (
-                    <img
-                        ref={postImagePreview}
-                        src={postImage}
-                        alt="preview"
-                        className="rounded-b"
-                    />
-                ) : null}
-
-                <FormLabel>Description</FormLabel>
-                <Textarea name="description" />
-                <Button type="submit">Upload</Button>
-            </form>
-        </Box>
-    );
-};
+import NewPostForm from "../../components/NewPostForm";
 
 const Profile = ({ initialProfile, posts: initialPosts }) => {
     const router = useRouter();
